1032d9468345d12baa6a8bd1f2cd36811e57a3fd
Ankit Saha <ankitsaha0517@gmail.com>
2025-03-30 23:54:46 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+
+let workContenter = document.querySelector(".work-container");
+
+const data = [
+    {
+        name: "Duo <br> Studio",
+        video: "./video/dustdio.mp4",
+        dec: "This is description",
+        projectLink: "https://ankitsaha0517.github.io/Duo-Studio-clone/"
+    },
+    {
+        name: "Sundown <br> Studio",
+        video: "./video/dustdio.mp4",
+        dec: "This is description",
+        projectLink: "https://ankitsaha0517.github.io/sundown-studio-clone/"
+    },
+    {
+        name: "Cynthia <br> Ugwu",
+        video: "./video/cynthea.mp4",
+        dec: "This is description",
+        projectLink: "https://ankitsaha0517.github.io/Cynthia-Ugwu-clone/"
+    },
+    {
+        name: "work",
+        video: "./video/work.mp4",
+        dec: "This is description",
+        projectLink: "https://github.com/ankitsaha0517/WORKS-studio-clone"
+    }
+
+];
+
+data.forEach((item) => {
+    workContenter.innerHTML += `
+        <div class="work">
+            <div class="work-name">
+              <h3>${item.name}</h3>
+            </div>
+            <div class="project ">
+              <video autoplay loop muted src="${item.video}"></video>
+            </div>
+            <div class="work-dec">
+              <p>${item.dec}</p>
+              <a class="btn" href="${item.projectLink}" target="_blank">View project</a>
+            </div>
+        </div>
+    `;
+});
+
+
+
+
+
+let a = document.querySelectorAll(".work");
+
+a.forEach((el) => {
+  el.addEventListener("mouseenter", function () {
+    let project = el.querySelector('.project')
+    project.classList.add('active')
+
+  });
+  el.addEventListener("mouseleave", function () {
+    let project = el.querySelector('.project')
+    project.classList.remove('active')
+
+  });
+});
